7629d8cb402985805b3c5735698bd78162d7b70e
Juno <sojae0306@gmail.com>
2025-05-26 18:49:04 +0900

    urls.py, models.py 수정

--- core/models.py
+++ core/models.py
@@ -6,3 +6,25 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+
+# Home model
+class Home(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Signin model
+class Signin(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+# Signup model
+class Signup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
